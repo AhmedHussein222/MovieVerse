dc073d254fdaf68dbf3f182ea40f3b59797d1969
Ahmed Hussein <Ahmdhussein222@gmail.com>
2025-03-17 11:06:06 +0200

    refactor routing structure to use createBrowserRouter and RouterProvider with Redux integration and add store in entire app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,62 @@
-import {   BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Movies from "./Component/Movies/Movies";
-import Header from "./Component/Header/Header";
 import Home from "./Pages/Home/Home";
 import Details from "./Component/Details/Details";
 import Favorite from "./Component/Favorite/Favorite";
+import Main from "./Component/Main/Main";
+import NotFound from "./Pages/Not Found/NotFound";
+import store from "./Store/Store";
+import { Provider } from "react-redux";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/Movies/:pageNumber",
+        element: <Movies />,
+      },
+      {
+        path: "/Details/:id",
+        element: <Details />,
+      },
+      {
+        path: "/Favorite",
+        element: <Favorite />,
+      },
+      {
+        path: "/Home",
+        element: <Home />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+      {/* <BrowserRouter>
     <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/Movies/:pageNumber" element={<Movies />} />
+          <Route path="/Movies/:pageNumber" element={<Movies />}  />
           <Route path="/Details/:id" element={<Details/>} />
           <Route path="/Favorite" element={<Favorite/>} />
         </Routes>
-    </BrowserRouter>
-        
+    </BrowserRouter> */}
+      <Provider store={store}>
+        <RouterProvider router={router}>
+          <Favorite />
+        </RouterProvider>
+      </Provider>
     </>
   );
 }
-
-
 
 export default App;
